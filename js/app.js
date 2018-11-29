26bf348c25533be54b1b91b6a522561fbaef6433
william contreras <sistemas@generalfirecontrol.com>
2018-11-29 17:17:47 -0500

    Inicio projecto

---
+++ js/app.js
@@ -0,0 +1,15 @@
+
+//lista de eventos teclas
+document.getElementById('1').addEventListener( "onclick",function () {
+alert("He presionado la tecla numero 1")
+})
+document.getElementById('on').addEventListener("click",function () {
+alert("He presionado la tecla ON")
+})
+//function teclas () {
+//document.getElementById("on").style="width:5px;"
+//}
+//document.getElementById('on').onclick= teclas,
+//function teclas(){
+//  document.write("He presionado la tecla ON")
+//}
